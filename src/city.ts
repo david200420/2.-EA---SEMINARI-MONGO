84edbce7d675178b0c2dc3f0188064bea4883ddd
david200420 <david.sanchez.company@estudiantat.upc.edu>
2025-09-29 20:30:02 +0200

    v

--- src/city.ts
+++ src/city.ts
@@ -1,7 +1,5 @@
 import { Schema, model, Types, ObjectId } from 'mongoose';
-//import { IUser } from './user.ts'; // Asegúrate de importar la interfaz IUser si está en otro archivo.
 
-// 1. Crear una interfaz que represente un objeto TS.
 export interface ICity {
   nombre: string;
   codigoPostal: string;
@@ -13,14 +11,13 @@ export interface ICity {
   _id?: Types.ObjectId;
 }
 
-// 2. Crear un esquema que corresponda al documento en MongoDB.
 const citySchema = new Schema<ICity>({
   nombre: { type: String, required: true },
   codigoPostal: { type: String, required: true },
   poblacion: { type: Number, required: true },
   pais: { type: String, required: true },
-  users: [{ user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true } }] // Array de referencias a usuarios
+  users: [{ user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true } }]
 });
 
-// 3. Crear un modelo.
+
 export const CityModel = model('City', citySchema);
--- src/mongoose.ts
+++ src/mongoose.ts
@@ -5,7 +5,7 @@ import { CityModel, ICity } from './city.js';
 async function main() {
   mongoose.set('strictQuery', true); // Mantiene el comportamiento actual
 
-  await mongoose.connect('mongodb://127.0.0.1:27017/testROCK')
+  await mongoose.connect('mongodb://127.0.0.1:27017/test')
   .then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.error('Error al conectar:', err));
 
@@ -13,18 +13,17 @@ async function main() {
     "name": 'Bill',
     "email": 'bill@initech.com',
     "avatar": 'https://i.imgur.com/dM7Thhn.png'
-    //"_id": "1"
   };
 
   console.log("user1", user1); 
   const newUser= new UserModel(user1);
-  const user2: IUser = await newUser.save();
+  const user2: IUser = await newUser.save();//convierte a MongoDB el objeto, y se espera a que se guerde para pasar a  la siguiente linea
   console.log("user2",user2);
   
   const exampleCity: ICity = {
-    "nombre": "Valencia",
-    "codigoPostal": "08001",
-    "poblacion": 1620343,
+    "nombre": "El Prat de Llobregat",
+    "codigoPostal": "08820",
+    "poblacion": 66.000,
     "pais": "España",
     "users": [
       { user_id : user2._id as Types.ObjectId }
@@ -39,11 +38,18 @@ async function main() {
   console.log("city", city);
 
   // Consulta para obtener los usuarios que viven en la ciudad
-  const cityWithUsers = await CityModel.findOne({ nombre: 'Valencia' })
+  const cityWithUsers = await CityModel.findOne({ nombre: 'El Prat de Llobregat' })
   .populate('users.user_id')
   .exec();
 
   console.log("cityWithUsers", cityWithUsers); 
+
+  //solo el email
+    const cityWithUsersemail = await CityModel.findOne({ nombre: 'El Prat de Llobregat' })
+  .populate('users.user_id','name email')
+  .exec();
+
+  console.log("cityWithUsersemail", cityWithUsersemail); 
 
   // Leer un usuario específico por su ID
   const usuario = await UserModel.findById(user2._id);
